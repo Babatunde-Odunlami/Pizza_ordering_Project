fa914e5a6a49fea4ee5330b3f194f389e73ed5e0
Babatunde-Odunlami <79424845+Babatunde-Odunlami@users.noreply.github.com>
2021-07-21 19:15:04 +0200

    Add files via upload

---
+++ Project4_Treasure_island_game.py
@@ -0,0 +1,39 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input("You're at a cross road, where do you wanna go? \n left or right? \n").lower()
+if choice1 == "left":
+	choice2= input('You just got to a river, do you wanna wait or swim? enter "wait" or "swim" \n ').lower()
+	if choice2 == "wait":
+		choice3 = input("You just arrived at a house, which door do you want to open? \n red, blue or yellow? \n").lower()
+		if choice3 == "yellow":
+			print("Congratulations!!! \n You made it to the treasure island")
+		else:
+			print("You entered a dragon\'s dungeon, Game over!")
+	else:
+		print("You were bitten by a crocodile, Game over!")
+else:
+	print("You fell into a hole, Game over!")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
